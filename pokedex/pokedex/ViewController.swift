2b361c7443fe4909940f480acdfc65fab4c42cc9
Cody Dubree <codydubree@MacBook-Pro-24.local>
2018-11-12 10:40:42 -0600

    started

---
+++ pokedex/pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Cody Dubree on 11/12/18.
+//  Copyright © 2018 Cody Dubree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
